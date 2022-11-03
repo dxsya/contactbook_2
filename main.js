7d7120b47f70f4b28aa4bbf78fef34cfcb2a3635
dxsya <dxgdxd@gmail.com>
2022-11-03 15:04:18 +0600

    add edit function and fixed other

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ let createContactModal = document.querySelector(".create-contact-modal");
 let content = document.querySelector(".content");
 let contactCreaterBtn = document.querySelector(".contact-creator-btn");
 let modalCloseBtn = document.querySelector(".modal-close-btn");
+let modalCloseBtn2 = document.querySelector(".modal-close-btn2");
+let saveContactBtn = document.querySelector(".contact-save-btn");
+let editModal = document.querySelector(".edit-contact-modal");
+console.log(editModal);
 
 //! search
 let searchInp = document.querySelector("#search");
@@ -15,14 +19,25 @@ let contactName = document.querySelector("#name");
 let contactEmail = document.querySelector("#email");
 let contactImg = document.querySelector("#img");
 
+// !инпуты для редактирования контакта
+let editContactName = document.querySelector("#editName");
+let editContactEmail = document.querySelector("#editEmail");
+let editContactImg = document.querySelector("#editImg");
+
 //! кнопка открытия модального окна для создаяния контакта
 createContactBtn.addEventListener("click", () => {
     createContactModal.style.display = "flex";
     content.style.display = "none";
 });
+
 //! просто крестик закрыть модалку
 modalCloseBtn.addEventListener("click", () => {
     createContactModal.style.display = "none";
+    content.style.display = "flex";
+});
+
+modalCloseBtn2.addEventListener("click", () => {
+    editModal.style.display = "none";
     content.style.display = "flex";
 });
 
@@ -47,8 +62,8 @@ contactCreaterBtn.addEventListener("click", async function () {
     render();
 });
 
+//! создание контактов
 let contactList = document.querySelector(".contacts");
-
 async function render() {
     let contacts = await fetch(`${API}?q=${searchVal}`)
         .then((res) => res.json())
@@ -56,8 +71,15 @@ async function render() {
     contactList.innerHTML = "";
     contacts.forEach((element) => {
         if (element.image == "") {
-            element.image = "./icons/iconAvatar.png";
+            element.image = "./icons/iconAvatar2.png";
         }
+        if (element.name == "") {
+            element.name = "/no name";
+        }
+        if (element.email == "") {
+            element.email = "/no email";
+        }
+
         let newContactCard = document.createElement("div");
         newContactCard.className = "contact-card";
         newContactCard.id = element.id;
@@ -69,19 +91,73 @@ async function render() {
                                 <p>${element.email}</p>
                             </div>
                             <div class="card-buttons">
-                                <button class="contact-edit">Edit</button>
-                                <button class="contact-delete" onclick='deleteContact(${element.id})'>Delete</button>
+                                <button id=${element.id} class="contact-edit">Edit</button>
+                                <button id=${element.id} class="contact-delete" onclick='deleteContact(${element.id})'>Delete</button>
                             </div>`;
         contactList.append(newContactCard);
     });
 }
 render();
 
+//! удаление контакта
 function deleteContact(id) {
     fetch(`${API}/${id}`, { method: "DELETE" }).then(() => render());
 }
 
+//! поиск контакта
 searchInp.addEventListener("input", () => {
     searchVal = searchInp.value;
     render();
 });
+
+//! редактирование контакта
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("contact-edit")) {
+        editModal.style.display = "flex";
+        content.style.display = "none";
+        createContactModal.style.display = "none";
+        let id = e.target.id;
+        fetch(`${API}/${id}`).then((res) =>
+            res.json().then((data) => {
+                editContactName.value = data.name;
+                editContactEmail.value = data.email;
+                editContactImg.value = data.image;
+                saveContactBtn.setAttribute("id", data.id);
+            })
+        );
+    }
+});
+
+//! сохранение контакта
+saveContactBtn.addEventListener("click", function () {
+    let id = this.id;
+    let name = editContactName.value;
+    let email = editContactEmail.value;
+    let img = editContactImg.value;
+    if (img == "") {
+        img = "./icons/iconAvatar2.png";
+    }
+    if (name == "") {
+        name = "/no name";
+    }
+    if (email == "") {
+        email = "/no email";
+    }
+
+    let editedContact = {
+        name: name,
+        email: email,
+        image: img,
+    };
+    saveContact(editedContact, id);
+});
+
+function saveContact(editedContact, id) {
+    fetch(`${API}/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json; carset=utf-8" },
+        body: JSON.stringify(editedContact),
+    }).then(() => render());
+    editModal.style.display = "none";
+    content.style.display = "flex";
+}
